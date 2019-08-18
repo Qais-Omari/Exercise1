d9a75ce55079e07cf0a69633e6c96b81639b65f3
DELL <DELL@DESKTOP-BHG0JJ3>
2019-08-18 12:40:18 +0300

    Add project files.

---
+++ Excercise1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Excercise1.MvcApplication" Language="C#" %>
---
+++ Excercise1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Excercise1.Startup))]
+namespace Excercise1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
